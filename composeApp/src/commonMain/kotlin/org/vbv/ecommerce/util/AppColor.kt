03bd3460fdf644b5afbe995442702bc9eb4553c1
Saumil <vbv193@gmail.com>
2025-06-28 21:53:06 +0530

    working on registration screen

--- composeApp/src/commonMain/kotlin/org/vbv/ecommerce/util/AppColor.kt
+++ composeApp/src/commonMain/kotlin/org/vbv/ecommerce/util/AppColor.kt
@@ -4,7 +4,12 @@ import androidx.compose.ui.graphics.Color
 
 object AppColor {
 
-    val colorWinAppBg = Color(26,26,26)
+    val colorWinAppBg = Color(26, 26, 26)
     val colorWhite = Color.White
-    val colorGrey = Color(153,153,153)
+    val colorTransparent = Color.Transparent
+    val colorHintGrey = Color(153, 153, 153)
+    val colorErrorRed = Color(237, 16, 16)
+    val colorTextGrey = Color(128, 128, 128)
+    val colorDividerGrey = Color(230, 230, 230)
+    val colorBlack = Color.Black
 }
